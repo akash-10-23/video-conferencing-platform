d5ad678bf64a33f3281244ad7beb6c2d35b3131b
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-10-26 23:53:06 +0530

    VideoCall component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inCall, setInCall] = useState(false);
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      {inCall ? "We are in a Call!" : "Waiting to join a call!"}
+    </div>
   );
 }
 
