c43280a30a980428a3cc17df10ee13ab9723360d
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-11-02 18:23:34 +0530

    generate access token for 1hr

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,21 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
+const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
 
 const appId = ""; // App ID from Agora
-const token = ""; // Temp Token 
+const appCertificate = ""; // App Ceritificate from Agora
+
+const uid = 0;
+const channelName = "main";
+const role = RtcRole.SUBSCRIBER; // default role subscriber
+const expireTime = 3600; //default expire time 1hr
+
+//calculate privilege expire time
+const currentTime = Math.floor(Date.now() / 1000);
+const privilegeExpireTime = currentTime + expireTime;
+
+const token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpireTime); // Temp Token 
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
-export const channelName = "main";
+export { channelName };
